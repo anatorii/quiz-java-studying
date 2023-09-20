0d12cde2ff4e2a83b5afb6974b991ab895ae2f13
anatoly.igumnov <anatoly.igumnov@gmail.com>
2023-09-20 14:10:33 +1100

    commit

--- src/Quiz.java
+++ src/Quiz.java
@@ -13,6 +13,7 @@ public class Quiz extends JFrame {
     JButton[] ba;
     JLabel label;
     JLabel question;
+//    JTextArea question;
 
     public Quiz() {
         super("Quiz");
@@ -92,19 +93,25 @@ public class Quiz extends JFrame {
         label.setBackground(Color.white);
         label.setOpaque(true);
 
-        question = new JLabel("question");
+        question = new JLabel("<html><body>fajfios gotm sgimrt gstmngstiomgso iegms;oirgtm se;rgioms e;goim seriogmse ;rgiomse;rogi ookp</body></html>");
+        question.setFocusable(false);
         question.setFont(new Font("Arial", Font.PLAIN, 20));
-        question.setHorizontalAlignment(SwingConstants.CENTER);
         question.setBackground(Color.white);
         question.setOpaque(true);
 
-        JButton ans1 = new JButton("a1");
-        JButton ans2 = new JButton("a2");
-        JButton ans3 = new JButton("a3");
-        JButton ans4 = new JButton("a4");
+        //        question = new JTextArea();
+//        question.setLineWrap(true);
+//        question.setWrapStyleWord(true);
+//        question.setFocusable(false);
+//        question.setEditable(false);
+//        question.setFont(new Font("Arial", Font.PLAIN, 20));
+//        question.setBackground(Color.white);
+//        question.setOpaque(true);
 
-        ba = new JButton[] {ans1, ans2, ans3, ans4};
-        for (int i = 0; i < 4; i++) {
+        ba = new JButton[4];
+        for (int i = 0; i < ba.length; i++) {
+            ba[i] = new JButton("button");
+//            ba[i] = new JButton("<html><body>fajfios gotm sgimrt gstmngstiomgso iegms;oirgtm se;rgioms e;goim seriogmse ;rgiomse;rogi ookp</body></html>");
             ba[i].setFont(new Font("Arial", Font.PLAIN, 20));
             ba[i].addActionListener(new AbstractAction() {
                 @Override
@@ -117,17 +124,16 @@ public class Quiz extends JFrame {
         this.add(label, new GridBagConstraints(0, 0, 4, 1, 1, 0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
                 new Insets(5,5,5,5), 5, 5));
         this.add(question, new GridBagConstraints(0, 1, 4, 1, 1, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-                new Insets(5,5,5,5), 5, 100));
+                new Insets(5,5,5,5), 0, 0));
 
-        this.add(ans1, new GridBagConstraints(0, 2, 2, 1, 1, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-                new Insets(5,5,5,5), 5, 100));
-        this.add(ans2, new GridBagConstraints(2, 2, 2, 1, 1, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-                new Insets(5,5,5,5), 5, 100));
-
-        this.add(ans3, new GridBagConstraints(0, 3, 2, 1, 1, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-                new Insets(5,5,5,5), 5, 100));
-        this.add(ans4, new GridBagConstraints(2, 3, 2, 1, 1, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-                new Insets(5,5,5,5), 5, 100));
+        this.add(ba[0], new GridBagConstraints(0, 2, 2, 1, 1, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+                new Insets(5,5,5,5), 0, 0));
+        this.add(ba[1], new GridBagConstraints(2, 2, 2, 1, 1, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+                new Insets(5,5,5,5), 0, 0));
+        this.add(ba[2], new GridBagConstraints(0, 3, 2, 1, 1, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+                new Insets(5,5,5,5), 0, 0));
+        this.add(ba[3], new GridBagConstraints(2, 3, 2, 1, 1, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+                new Insets(5,5,5,5), 0, 0));
 
         this.setVisible(true);
 
@@ -158,7 +164,7 @@ public class Quiz extends JFrame {
 
     public void setQuestion() {
         label.setText("Вопрос " + (curQuestion + 1) + "/" + q.length);
-        question.setText(q[curQuestion][0]);
+        question.setText("<html><body>" + q[curQuestion][0] + "</body></html>");
         for (int i = 0; i < 4; i++) {
             ba[i].setText(q[curQuestion][i+1]);
         }
